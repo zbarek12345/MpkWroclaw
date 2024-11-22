94db44ca39555d88c7865c9079b5a2731ab85e8f
zbarek12345 <zbarek12345@gmail.com>
2024-11-22 19:52:16 +0100

    Refactor vehicle methods for better route management
    Renamed vehiclesOnStop() to vehiclesForStop() and implemented detailed logic to fetch stop names and associated routes. Added getRoutesForStop() to retrieve distinct route IDs and getNextVehicles() placeholder for future vehicle arrival logic. Updated Main method to reflect the new method signature.

--- Database/MpkSingleton.cs
+++ Database/MpkSingleton.cs
@@ -8,10 +8,54 @@ public class MpkSingleton
         _databaseContext = databaseContext;
     }
 
-    public string[] vehiclesOnStop(int stop_id)
+    public string vehiclesForStop(int stop_id)
     {
-        var vehicles = _databaseContext.Trips;
-        return null;
+        var stopName = _databaseContext.Stops.First(e => e.stop_id == stop_id).stop_name;
+        var trips = _databaseContext.StopTimes.Where(e => e.stop_id == stop_id);
+        
+        var tripIds = trips.Select(t => t.trip_id).ToList();
+        
+        var uniqueRouteIds = _databaseContext.Trips
+            .Where(r => tripIds.Contains(r.trip_id))
+            .Select(r => r.route_id)
+            .Distinct()
+            .ToList();
+        var uniqueRouteHeadisgns = _databaseContext.Trips
+            .Where(r => tripIds.Contains(r.trip_id))
+            .Select(r => r.trip_headsign)
+            .Distinct()
+            .ToList();
+
+        var retS = stopName + ":\n"; 
+        
+        for (int i = 0; i < uniqueRouteIds.Count; i++)
+        {
+            retS += uniqueRouteIds[i] + " -> " + uniqueRouteHeadisgns[i]+'\n';
+        }
+
+        return retS;
     }
     
+    public List<string> getRoutesForStop(int stop_id)
+    {
+        var trips = _databaseContext.StopTimes.Where(e => e.stop_id == stop_id).Select(t => t.trip_id).ToList();
+
+        var routes = _databaseContext.Trips.Where(t => trips.Contains(t.trip_id))
+            .Select(r => r.route_id)
+            .Distinct()
+            .ToList();
+
+        return routes;
+    }
+
+    public string getNextVehicles(int stop_id, int count)
+    {
+        var trips = _databaseContext.StopTimes.Where(e => e.stop_id == stop_id && e.arrival_time>=).Select(t => t.trip_id).ToList();
+    }
+
+    public static void Main()
+    {
+        MpkSingleton mpkSingleton = new MpkSingleton(new MpkDatabaseContext());
+        Console.Write(mpkSingleton.vehiclesForStop(235));
+    }
 }
